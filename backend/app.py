aae945c4f47f7ce2c85dd6345c3b55ed220efff4
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-08-26 19:27:31 -0500

    fixes to project structure

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,8 @@ Session(app)
 
 CORS(app, supports_credentials=True, origins=[
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://tainoheritagecamp.netlify.app/"
 ])
 
 questions = [
