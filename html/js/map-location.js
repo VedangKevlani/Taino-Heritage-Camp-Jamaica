74bb598cf65396ebc894864cab3352a612464d86
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-08-29 15:07:33 -0500

    media queries for smaller viewports index.html

--- html/js/map-location.js
+++ html/js/map-location.js
@@ -1,6 +1,12 @@
- const openBtn = document.getElementById("openLightbox");
-    const closeBtn = document.getElementById("closeLightbox");
-    const lightbox = document.getElementById("lightbox");
+const openBtn = document.getElementById("openLightbox");
+const closeBtn = document.getElementById("closeLightbox");
+const lightbox = document.getElementById("lightbox");
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
 
     openBtn.addEventListener("click", () => {
       lightbox.style.display = "flex"; // show lightbox
