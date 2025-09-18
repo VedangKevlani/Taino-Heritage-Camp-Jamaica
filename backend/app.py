6c1a498c63d3fafb809889e0672ea73f4cd63d49
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-09-18 11:10:04 -0500

    updated sender email

--- backend/app.py
+++ backend/app.py
@@ -328,7 +328,7 @@ def email_ticket_multi(recipients, pdf_file, subject="Your Taino Heritage Camp T
     try:
         port = 465
         smtp_server = "smtp.gmail.com"
-        sender_email = "vibranzmagazine@gmail.com"
+        sender_email = "tainoheritagecamp@gmail.com"
         password = os.getenv("EMAIL_PASS")  # must be set
 
         if not password:
